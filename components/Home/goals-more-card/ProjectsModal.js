58c0934d8fb6a875a5f501a49b2898aebebb3424
David Guri <Davidguri@yahoo.com>
2022-02-17 17:36:19 +0100

    .

--- components/Home/goals-more-card/ProjectsModal.js
+++ components/Home/goals-more-card/ProjectsModal.js
@@ -14,7 +14,86 @@ import {
 import Colors from "../../../constants/colors";
 import Card from "../../Other/Global/Card";
 
+const DATA = [
+  {
+    id: "1",
+    title: "Mid-Term Project",
+    subject: "Math",
+    date: "16/02/2022",
+    work: {
+      "first-task": "Do this",
+      "second-task": "Do that",
+    },
+    team: ["arlind-gorrea", "bjorni-hoxha", "david-guri", "kristjan-avrami"],
+    points: {
+      "first-task": 10,
+      "second-task": 10,
+    },
+  },
+  {
+    id: "2",
+    title: "Term Project",
+    subject: "English",
+    date: "16/02/2022",
+    work: {
+      "first-task": "Do this",
+      "second-task": "Do that",
+    },
+    team: ["arlind-gorrea", "bjorni-hoxha", "david-guri", "kristjan-avrami"],
+    points: {
+      "first-task": 10,
+      "second-task": 10,
+    },
+  },
+];
+
+const Item = ({ title, subject, date, work, team, points }) => {
+  {
+    /* const str = "hi-i-am-david-guri";
+  const arr = str.split("-");
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+  }
+  const str2 = arr.join(" ");
+console.log(str2); */
+  }
+  {
+    /* This is just a proof of concept for now to turn the names above into a normal convention */
+  }
+  {
+    /* const TotalPoints = {
+    In here, have a function that just adds the points from each of the tasks
+  }; */
+  }
+
+  return (
+    <View style={styles.item}>
+      <Card style={styles.projectCard}>
+        <View style={styles.cardHeader}>
+          <View style={styles.headerTop}>
+            <Text style={styles.projectTitle}>{title}</Text>
+            <Text style={styles.projectSubject}>{subject}</Text>
+          </View>
+          <View style={styles.headerBottom}>
+            <Text style={styles.projectSubtitle}>{date}</Text>
+          </View>
+        </View>
+        <View style={styles.cardBody}>
+          <View style={[styles.teamContainer, styles.container]}></View>
+          <View style={[styles.workContainer, styles.container]}></View>
+        </View>
+        <View style={styles.cardFooter}></View>
+      </Card>
+    </View>
+  );
+};
+{
+  /* Gonna have to make two modals: one for seeing the team and another for the work/points */
+}
+
 const ProjectsModal = (props) => {
+  const renderItem = ({ item }) => <Item title={item.title} />;
+
   return (
     <Modal visible={props.visible} animationType="slide" transparent={true}>
       <SafeAreaView style={{ backgroundColor: "black" }}>
@@ -28,7 +107,15 @@ const ProjectsModal = (props) => {
               <Text style={styles.buttonCancelText}>X</Text>
             </TouchableOpacity>
           </View>
-          <View style={styles.body}></View>
+          <View style={styles.body}>
+            <FlatList
+              data={DATA}
+              renderItem={renderItem}
+              keyExtractor={(item) => item.id}
+              style={styles.list}
+              scrollEnabled={false}
+            />
+          </View>
         </View>
       </SafeAreaView>
     </Modal>
@@ -112,6 +199,43 @@ const styles = StyleSheet.create({
 
   footer: {
     width: "100%",
+  },
+
+  projectCard: {
+    borderWidth: 4,
+    borderRadius: 25,
+    borderColor: Colors.primary,
+    width: "100%",
+    padding: 0,
+    paddingBottom: 15,
+  },
+
+  cardHeader: {
+    flexDirection: "column",
+    backgroundColor: Colors.primary,
+    borderRadius: 20,
+    padding: 12,
+  },
+
+  headerTop: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingBottom: 3,
+  },
+
+  headerBottom: {
+    paddingVertical: 3,
+  },
+
+  projectTitle: {
+    fontSize: 18,
+    fontWeight: "500",
+  },
+
+  projectSubtitle: {
+    fontSize: 17,
+    fontWeight: "500",
   },
 });
 
