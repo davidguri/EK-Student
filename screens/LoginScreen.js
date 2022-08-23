2874765c71f0632cac30487d9497345d363096cd
David Guri <Davidguri@yahoo.com>
2022-08-23 14:55:22 +0200

    ...

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,24 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import {
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
-  TouchableWithoutFeedback,
-  Keyboard,
   SafeAreaView,
+  Keyboard,
+  ImageBackground,
+  TouchableWithoutFeedback,
 } from "react-native";
-
-import LoginHead from "../components/Login/LoginHead";
-import LoginModal from "../components/Login/LoginModal";
-
 import Colors from "../constants/colors";
+import Modal from "react-native-modal";
+import { AppContext } from "../App";
+
+import Input from "../components/Other/Global/Input";
+import LoginHead from "../components/Login/LoginHead";
+import SignupModal from "../components/Login/SignupModal";
+
+import Background from "../assets/background.png";
+
+import { auth } from "../firebase";
 
 const LoginScreen = (props) => {
-  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [isOpenLogin, setIsOpenLogin] = useState(false);
 
-  const toggleModalHandler = () => {
-    setIsOpenModal(!isOpenModal);
+  const toggleLoginHandler = () => {
+    setIsOpenLogin(!isOpenLogin);
+  };
+
+  const [isOpenSignup, setIsOpenSignup] = useState(false);
+
+  const toggleSignupHandler = () => {
+    setIsOpenSignup(!isOpenSignup);
+  };
+  // Login Shizz
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { setIsSignedIn } = useContext(AppContext);
+
+  const handleLogin = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log("Logged in with:", user.email);
+        setIsSignedIn(true);
+      })
+      .catch((error) => alert(error.message));
   };
 
   return (
@@ -28,28 +58,113 @@ const LoginScreen = (props) => {
       }}
     >
       <SafeAreaView style={styles.screen}>
-        <LoginHead title="WELCOME" />
-        <View style={styles.loginForm}>
-          <View style={styles.titleContainer}>
-            <Text style={styles.title}>Welcome To</Text>
-            <Text style={styles.titleAlt}>EK Student!</Text>
+        <ImageBackground
+          source={Background}
+          resizeMode="cover"
+          style={styles.image}
+        >
+          <LoginHead title="WELCOME" />
+          <View style={styles.loginForm}>
+            <View style={styles.titleContainer}>
+              <Text style={styles.title}>Welcome To</Text>
+              <Text style={styles.titleAlt}>EK Student!</Text>
+            </View>
+            <View style={styles.actionsContainer}>
+              <TouchableOpacity
+                onPress={toggleLoginHandler}
+                style={styles.buttonContainer}
+              >
+                <View style={styles.ctaButton}>
+                  <Text style={styles.buttonText}>Log In</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={toggleSignupHandler}
+                style={styles.buttonContainer}
+              >
+                <View style={styles.ctaButton}>
+                  <Text style={styles.buttonText}>Sign Up</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
           </View>
-          <View style={styles.actionsContainer}>
-            <TouchableOpacity
-              onPress={toggleModalHandler}
-              style={styles.buttonContainer}
-            >
-              <View style={styles.ctaButton}>
-                <Text style={styles.buttonText}>Log In</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-        </View>
-        <LoginModal
-          visible={isOpenModal}
-          onCancel={toggleModalHandler}
-          loginPress={props.loginPress}
-        />
+          <Modal
+            isVisible={isOpenLogin}
+            backgroundColor={"#000"}
+            style={{ margin: 0 }}
+            hideModalContentWhileAnimating={true}
+            onBackdropPress={props.onBackdropPress}
+            animationIn={"slideInRight"}
+            animationInTiming={350}
+            animationOutTiming={350}
+            avoidKeyboard={true}
+            onSwipeComplete={toggleLoginHandler}
+            swipeDirection="right"
+          >
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+              <SafeAreaView style={styles.media}>
+                <ImageBackground
+                  source={Background}
+                  resizeMode="cover"
+                  style={styles.image}
+                >
+                  <LoginHead title="LOG IN" />
+                  <View style={styles.loginForm}>
+                    <View style={styles.titleContainerModal}>
+                      <Text style={styles.titleModal}>Log In</Text>
+                    </View>
+                    <View style={styles.inputContainer}>
+                      <Input
+                        style={styles.input}
+                        blurOnSubmit
+                        autoCorrect={false}
+                        placeholder="jdoe@ernestkoliqi.com"
+                        placeholderTextColor="#999"
+                        value={email}
+                        onChangeText={(text) => setEmail(text)}
+                        selectTextOnFocus
+                        keyboardType="email-address"
+                      />
+                      <Input
+                        style={styles.input}
+                        blurOnSubmit
+                        autoCorrect={false}
+                        placeholder="Password"
+                        placeholderTextColor="#999"
+                        bool={true}
+                        value={password}
+                        onChangeText={(text) => setPassword(text)}
+                        selectTextOnFocus
+                      />
+                    </View>
+                    <View style={styles.buttonContainerModal}>
+                      <TouchableOpacity onPress={handleLogin}>
+                        <View style={styles.ctaBtnModal}>
+                          <Text style={styles.loginBtn}>Log In</Text>
+                        </View>
+                      </TouchableOpacity>
+                      <TouchableOpacity onPress={() => {}}>
+                        <View style={styles.forgotBtn}>
+                          <Text
+                            style={{
+                              color: "white",
+                              fontSize: 17,
+                              paddingRight: 5,
+                              fontWeight: "500",
+                            }}
+                          >
+                            Forgot Password?
+                          </Text>
+                        </View>
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                </ImageBackground>
+              </SafeAreaView>
+            </TouchableWithoutFeedback>
+          </Modal>
+          <SignupModal visible={isOpenSignup} onCancel={toggleSignupHandler} />
+        </ImageBackground>
       </SafeAreaView>
     </TouchableWithoutFeedback>
   );
@@ -59,6 +174,11 @@ const styles = StyleSheet.create({
   screen: {
     height: "100%",
     width: "100%",
+    justifyContent: "center",
+  },
+
+  image: {
+    flex: 1,
     justifyContent: "center",
   },
 
@@ -96,9 +216,10 @@ const styles = StyleSheet.create({
 
   actionsContainer: {
     flexDirection: "column",
-    justifyContent: "space-around",
+    justifyContent: "space-between",
     alignItems: "center",
-    paddingTop: 25,
+    paddingVertical: 25,
+    paddingHorizontal: 7.5,
   },
 
   buttonContainer: {
@@ -106,8 +227,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    padding: 18,
-    paddingHorizontal: 25,
+    padding: 10,
   },
 
   ctaButton: {
@@ -128,6 +248,89 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 23,
     color: "#fff",
+  },
+
+  container: {
+    flex: 1,
+  },
+
+  media: {
+    height: "100%",
+    width: "100%",
+    justifyContent: "center",
+    backgroundColor: "#000",
+  },
+
+  titleContainerModal: {
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+
+  titleModal: {
+    fontSize: 42,
+    fontWeight: "bold",
+    color: "#fff",
+    paddingTop: 75,
+    paddingBottom: 25,
+    textAlign: "center",
+  },
+
+  loginForm: {
+    width: "100%",
+    height: "90%",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingBottom: 75,
+  },
+
+  inputContainer: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  input: {
+    width: "85%",
+    textAlign: "center",
+    padding: 10,
+    paddingHorizontal: 14,
+    fontSize: 14,
+    borderWidth: 4,
+    color: "white",
+    textAlign: "left",
+  },
+
+  buttonContainerModal: {
+    width: "85%",
+    flexDirection: "row",
+    paddingVertical: 10,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  ctaBtnModal: {
+    width: "100%",
+    backgroundColor: Colors.primary,
+    fontWeight: "600",
+    borderRadius: 18,
+    padding: 4,
+    paddingHorizontal: 30,
+    marginHorizontal: 0,
+  },
+
+  loginBtn: {
+    fontWeight: "600",
+    paddingVertical: 8.7,
+    paddingHorizontal: 8,
+    textAlign: "center",
+    fontSize: 17.5,
+  },
+
+  forgotBtn: {
+    width: "100%",
+    paddingVertical: 5,
+    opacity: 0.65,
   },
 });
 
