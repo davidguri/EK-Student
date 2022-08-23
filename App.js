47fbabd7b3179ebc91397adb27e24c71e5362c2b
David Guri <Davidguri@yahoo.com>
2022-08-23 14:55:31 +0200

    ...

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
 import React, { useState } from "react";
 import { StyleSheet, SafeAreaView, StatusBar } from "react-native";
-// import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
-import { enableScreens } from "react-native-screens";
 
 import Colors from "./constants/colors";
-
 import LoginScreen from "./screens/LoginScreen";
 import HomeScreen from "./screens/HomeScreen";
 import NewsScreen from "./screens/NewsScreen";
 import CalendarScreen from "./screens/CalendarScreen";
-
-import firebase from "firebase/compat/app";
 
 const appTheme = {
   dark: true,
@@ -40,75 +35,89 @@ function Calendar() {
   return <CalendarScreen />;
 }
 
-enableScreens();
+function Login() {
+  return <LoginScreen />;
+}
+
+function AppScreen() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "News") {
+            iconName = focused ? "newspaper" : "newspaper-outline";
+          } else if (route.name === "Calendar") {
+            iconName = focused ? "calendar" : "calendar-outline";
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        activeTintColor: Colors.primary,
+        inactiveTintColor: "gray",
+        showLabel: true,
+        showIcon: true,
+        style: {
+          paddingBottom: 4.25,
+          paddingTop: 4.25,
+        },
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Calendar"
+        component={Calendar}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="News"
+        component={News}
+        options={{ headerShown: false }}
+      />
+    </Tab.Navigator>
+  );
+}
+
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
+export const AppContext = React.createContext({});
+
 export default function App() {
-  const [isLogged, setIsLogged] = useState(false);
+  const [isSignedIn, setIsSignedIn] = useState(true);
+  const contextValue = React.useMemo(
+    () => ({
+      isSignedIn,
+      setIsSignedIn,
+    }),
+    [isSignedIn]
+  );
 
-  // var user = firebase.auth().currentUser;
-
-  // setTimeout(() => {
-  //   user != null ? setIsLogged(true) : setIsLogged(false);
-  // }, 1000);
-
-  const logInHandler = () => {
-    var user = firebase.auth().currentUser;
-
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        console.log("Logged In Message Given. Noice.");
-        setIsLogged(true);
-      }
-    });
-  };
-
-  if (isLogged == true) {
-    return (
-      <SafeAreaView style={styles.screen}>
-        <StatusBar barStyle="light-content" translucent={true} />
+  return (
+    <SafeAreaView style={styles.screen}>
+      <StatusBar barStyle="light-content" translucent={true} />
+      <AppContext.Provider value={contextValue}>
         <NavigationContainer theme={appTheme}>
-          <Tab.Navigator
-            screenOptions={({ route }) => ({
-              tabBarIcon: ({ focused, color, size }) => {
-                let iconName;
-                if (route.name === "Home") {
-                  iconName = focused ? "home" : "home-outline";
-                } else if (route.name === "News") {
-                  iconName = focused ? "newspaper" : "newspaper-outline";
-                } else if (route.name === "Calendar") {
-                  iconName = focused ? "calendar" : "calendar-outline";
-                }
-                return <Ionicons name={iconName} size={size} color={color} />;
-              },
-            })}
-            tabBarOptions={{
-              activeTintColor: Colors.primary,
-              inactiveTintColor: "gray",
-              showLabel: true,
-              showIcon: true,
-              style: {
-                paddingBottom: 4.25,
-                paddingTop: 4.25,
-              },
-            }}
-          >
-            <Tab.Screen name="Home" component={Home} options={{ headerShown: false }} />
-            <Tab.Screen name="Calendar" component={Calendar} options={{ headerShown: false }} />
-            <Tab.Screen name="News" component={News} options={{ headerShown: false }} />
-          </Tab.Navigator>
+          <Stack.Navigator>
+            {!isSignedIn ? (
+              <Stack.Screen
+                component={Login}
+                options={{ headerShown: false }}
+                name="Login"
+              />
+            ) : (
+              <Stack.Screen component={AppScreen} options={{ headerShown: false }} />
+            )}
+          </Stack.Navigator>
         </NavigationContainer>
-      </SafeAreaView>
-    );
-  } else {
-    return (
-      <SafeAreaView style={styles.screen}>
-        <StatusBar barStyle="light-content" translucent={true} />
-        <LoginScreen loginPress={logInHandler} />
-      </SafeAreaView>
-    );
-  }
+      </AppContext.Provider>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
