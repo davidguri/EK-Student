321507fea003162c713398e6d2be535afbab8f59
David Guri <Davidguri@yahoo.com>
2022-09-17 11:32:57 +0200

    Created Template Files for later

---
+++ TEMPLATE-MODAL.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+} from "react-native";
+import Modal from "react-native-modal";
+
+import Colors from "../../constants/colors";
+import Card from "../Other/Global/Card";
+
+
+const TemplateModal = (props) => {
+  return (
+    <Modal
+      isVisible={props.visible}
+      animationIn={"slideInUp"}
+      animationOut={"slideOutDown"}
+      style={{ margin: 0 }}
+      hideModalContentWhileAnimating={true}
+    >
+      <SafeAreaView style={{ backgroundColor: "black" }}>
+        <View style={styles.screen}>
+          
+        </View>
+      </SafeAreaView>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: "#000",
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    padding: 8.5,
+  },
+});
+
+export default TemplateModal;
