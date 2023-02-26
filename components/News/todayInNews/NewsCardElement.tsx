4806135af91df80b82fe40fd187b7e9872243172
davidguri <david.guri@icloud.com>
2023-02-26 17:02:05 +0100

    Trying to get the news card to work

--- components/News/todayInNews/NewsCardElement.tsx
+++ components/News/todayInNews/NewsCardElement.tsx
@@ -5,54 +5,39 @@ import Card from "../../Other/Global/Card";
 import Colors from "../../../constants/colors";
 
 export default function NewsCardElement(props): any {
-    return (
-        <Card style={styles.newsCard}>
-            <View style={styles.main}>
-                <Text style={styles.newsTitle}>{props.title}</Text>
-                <Text style={styles.newsDesc}>Hello there I am david guri</Text>
-                <View style={styles.newsInfoContainer}>
-                    <Text style={styles.newsInfo}>{props.publisher}, {props.date}</Text>
-                </View>
-            </View>
-        </Card>
-    );
+  return (
+    <Card style={styles.newsCard}>
+      <Text style={styles.newsTitle}>{props.title}</Text>
+      <Text style={styles.newsDesc}>Hello there I am david guri</Text>
+      <Text style={styles.newsInfo}>{props.publisher}, {props.date}</Text>
+    </Card>
+  );
 };
 
 const styles = StyleSheet.create({
-    newsCard: {
-        alignItems: "flex-start",
-        justinfyContent: "center",
-        marginVertical: 20,
-        padding: 25,
-        backgroundColor: Colors.primary,
-        borderRadius: 25,
-        width: "95%"
-    },
+  newsCard: {
+    alignItems: "flex-start",
+    justinfyContent: "center",
+    paddingVertical: 25,
+    backgroundColor: Colors.primary,
+    borderRadius: 25,
+    width: "95%"
+  },
 
-    main: {
-        width: "100%"
-    },
+  newsTitle: {
+    fontSize: 39,
+    fontWeight: "900",
+    marginBottom: 5,
+  },
 
-    newsTitle: {
-        fontSize: 39,
-        fontWeight: "900",
-        marginBottom: 4,
-    },
+  newsDesc: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginBottom: 20,
+  },
 
-    newsDesc: {
-        fontSize: 18,
-        fontWeight: "600",
-        marginBottom: 18,
-    },
-
-    newsInfoContainer: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-    },
-
-    newsInfo: {
-        fontSize: 14,
-        fontWeight: "600",
-    },
+  newsInfo: {
+    fontSize: 14,
+    fontWeight: "600",
+  },
 });
