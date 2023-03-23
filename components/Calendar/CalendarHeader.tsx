0ff8f535fe50a92986c8719bf8ba562734d575ac
davidguri <david.guri@icloud.com>
2023-03-23 21:20:27 +0100

    Added new feature to calendar

--- components/Calendar/CalendarHeader.tsx
+++ components/Calendar/CalendarHeader.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
   StyleSheet,
   StatusBar,
   Platform,
+  TouchableHighlight
 } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "../../constants/colors";
 
 export default function CalendarHeader(props): any {
+
+  const [selected, setSelected] = useState(1);
+
+  let iconColor1: string, iconColor2: string;
+
+  if (selected == 1) {
+    iconColor1 = "black";
+    iconColor2 = Colors.primary;
+  } else {
+    iconColor2 = "black";
+    iconColor1 = Colors.primary;
+  }
+
   let months = [
     "Jan",
     "Feb",
@@ -44,15 +60,50 @@ export default function CalendarHeader(props): any {
 
   return (
     <View style={styles.header}>
-      <Text style={styles.headerTitle}>Today,</Text>
-      <Text style={styles.headerSubtitle}>{date}</Text>
+      <View style={{}}>
+        <Text style={styles.headerTitle}>Today,</Text>
+        <Text style={styles.headerSubtitle}>{date}</Text>
+      </View>
+      <View style={styles.headerButtonContainer}>
+        <TouchableHighlight
+          onPress={() => setSelected(1)}
+          style={[
+            styles.buttonLeftContainer,
+            selected === 1 ? { backgroundColor: Colors.primary } : null,
+          ]}
+        >
+          <Ionicons
+            name="calendar"
+            color={iconColor1}
+            size={25}
+            style={{ padding: 5 }}
+          />
+        </TouchableHighlight>
+        <TouchableHighlight
+          onPress={() => setSelected(2)}
+          style={[
+            styles.buttonRightContainer,
+            selected === 2 ? { backgroundColor: Colors.primary } : null,
+          ]}
+        >
+          <Ionicons
+            name="apps"
+            color={iconColor2}
+            size={25}
+            style={{ padding: 5 }}
+          />
+        </TouchableHighlight>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   header: {
+    width: "100%",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    flexDirection: "row",
+    alignItems: "center",
     justifyContent: "space-between",
   },
 
@@ -73,5 +124,35 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: "bold",
     paddingBottom: 12,
+  },
+
+  headerButtonContainer: {
+    width: "auto",
+    flexDirection: "row",
+    height: "auto",
+  },
+
+  buttonLeftContainer: {
+    borderWidth: 2.5,
+    borderRightWidth: 1.35,
+    borderColor: Colors.primary,
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 4.75,
+    paddingHorizontal: 5.3,
+  },
+
+  buttonRightContainer: {
+    borderWidth: 2.5,
+    borderLeftWidth: 1.35,
+    borderColor: Colors.primary,
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 4.75,
+    paddingHorizontal: 5.3,
   },
 });
