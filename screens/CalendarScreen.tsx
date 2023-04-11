93761ee97e1c9430341b3a52e7360aaae8cb11f7
davidguri <david.guri@icloud.com>
2023-04-11 19:56:39 +0200

    Added event displaying functionality

--- screens/CalendarScreen.tsx
+++ screens/CalendarScreen.tsx
@@ -7,57 +7,69 @@ import CalendarHeader from "../components/Calendar/CalendarHeader";
 
 import { Calendar } from "react-native-calendars";
 
+const events = [
+  { title: 'Event 1', date: '2023-04-01' },
+  { title: 'Event 2', date: '2023-04-01' },
+  { title: 'Event 3', date: '2023-04-02' },
+  { title: 'Event 4', date: '2023-04-03' },
+];
+
 const CalendarObject = () => {
-  const daysOff = { key: "dayOff", color: Colors.daysOff.main, selectedDotColor: Colors.daysOff.main }
-  const classes = { key: "classes", color: Colors.classes.main, selectedDotColor: Colors.classes.main }
-  const tests = { key: "tests", color: Colors.tests.main, selectedDotColor: Colors.tests.main }
-  const events = { key: "events", color: Colors.events.main, selectedDotColor: Colors.events.main }
+  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().slice(0, 10));
+
+  const getEventsForDay = (date: any) => {
+    return events.filter((event) => event.date === date);
+  };
+
+  const onDayPress = (day: any) => {
+    setSelectedDate(day.dateString);
+  };
 
   let currentDate = (new Date()).toISOString().split('T')[0]
+
   return (
-    <Calendar
-      enableSwipeMonths={true}
-      minDate={"2023-01-01"}
-      maxDate={"2023-06-16"}
-      futureScrollRange={9}
-      pastScrollRange={9}
-      firstDay={1}
-      current={currentDate}
-      onDayPress={day => {
-        console.log("Selected day: ", day.dateString)
-      }}
-      disableAllTouchEventsForDisabledDays={true}
-      displayLoadingIndicator={false}
-      markingType={"multi-dot"}
-      markedDates={{
-        "2023-04-22": { dots: [events, daysOff], textColor: "black" },
-        "2023-04-23": { color: Colors.tests.main, textColor: "black" },
-        "2023-04-24": { color: Colors.events.main, textColor: "black" },
-        "2023-04-25": { dots: [classes, tests] },
-      }}
-      style={{
-        height: "100%",
-        backgroundColor: "#000",
-      }}
-      theme={{
-        backgroundColor: "#000",
-        calendarBackground: "#000",
-        textSectionTitleColor: "#b6c1cd",
-        todayTextColor: Colors.accent,
-        dayTextColor: "#d9e1e8",
-        textDisabledColor: "#2d4150",
-        monthTextColor: Colors.primary,
-        indicatorColor: "white",
-        textDayFontWeight: "500",
-        textMonthFontWeight: "700",
-        textDayFontSize: 18,
-        textMonthFontSize: 34,
-        textDayHeaderFontSize: 14,
-        textDayHeaderFontWeight: "700",
-        arrowColor: "#fff",
-        disabledArrowColor: '#2d4150',
-      }}
-    />
+    <View style={{ height: "100%", width: "100%" }}>
+      <Calendar
+        enableSwipeMonths={true}
+        minDate={"2023-01-01"}
+        maxDate={"2023-06-16"}
+        futureScrollRange={9}
+        pastScrollRange={9}
+        firstDay={1}
+        current={currentDate}
+        onDayPress={onDayPress}
+        disableAllTouchEventsForDisabledDays={true}
+        displayLoadingIndicator={false}
+        markingType={"multi-dot"}
+        markedDates={{}}
+        style={{
+          height: "100%",
+          backgroundColor: "#000",
+        }}
+        theme={{
+          backgroundColor: "#000",
+          calendarBackground: "#000",
+          textSectionTitleColor: "#b6c1cd",
+          todayTextColor: Colors.accent,
+          dayTextColor: "#d9e1e8",
+          textDisabledColor: "#2d4150",
+          monthTextColor: Colors.primary,
+          indicatorColor: "white",
+          textDayFontWeight: "500",
+          textMonthFontWeight: "700",
+          textDayFontSize: 18,
+          textMonthFontSize: 34,
+          textDayHeaderFontSize: 14,
+          textDayHeaderFontWeight: "700",
+          arrowColor: "#fff",
+          disabledArrowColor: '#2d4150',
+        }}
+      />
+      <Text style={{ color: "white", fontSize: 15 }}>Events for {selectedDate}:</Text>
+      {getEventsForDay(selectedDate).map((event, index) => (
+        <Text style={{ color: "white", fontSize: 15 }} key={index}>{event.title}</Text>
+      ))}
+    </View>
   );
 }
 
